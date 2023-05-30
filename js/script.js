63c6a1c574eb4e6aa78fa3726e27ccf6cac34dc0
micajermak <mbjermak@gmail.com>
2023-05-30 15:55:51 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function abrirmenu() {
+    const menu = document.querySelector("#menu");
+    menu.style.display = 'block';
+}
+
+function cerrarmenu() {
+    const menu = document.querySelector("#menu");
+    menu.style.display = 'none';
+}
+
